a489679d6f77591accdc216cb4e50e8fc7b32e82
Matt Bowen <mattbowen03@gmail.com>
2023-12-30 21:45:51 -0600

    add template inheritance

--- projects/views.py
+++ projects/views.py
@@ -11,3 +11,10 @@ def all_projects(request):
     # query the db to return all project objects
     projects = Project.objects.all()
     return render(request, 'projects/all_projects.html', {'projects': projects})
+
+
+def project_detail(request, pk):
+    # query the db to return all project objects
+    project = Project.objects.get(pk=pk)
+    return render(request, 'projects/detail.html', {'project': project})
+
